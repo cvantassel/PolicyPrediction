a3df0bcbc881ff3a1f89ab8f64cec3aa96c89a77
Conraw <46946984+Conraw@users.noreply.github.com>
2020-11-14 14:08:32 -0600

    Filtering out locations

--- policyPrediction.py
+++ policyPrediction.py
@@ -2,6 +2,15 @@ import pandas as pd
 import re
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+import seaborn as sns
+plt.style.use("fivethirtyeight")
+sns.set()
+sns.palplot(sns.color_palette())
+
+
 
 df = pd.read_csv("covid19_tweets.csv")
 
@@ -23,3 +32,67 @@ nltk.download('vader_lexicon')
 sid = SentimentIntensityAnalyzer()
 df["Score"] = df["text_clean"].apply(lambda x: sid.polarity_scores(x)) 
 print(df["Score"])
+
+print("No. Of Original Unique Locations:",df["user_location"].nunique())
+
+df.dropna(subset=['user_location'], inplace=True)
+
+#if (df['user_location'].__contains__('India')):
+#    df['user_location'] = 'India'
+
+
+df.loc[df['user_location'].str.contains('India', case=False)] = "India"
+df.loc[df['user_location'].str.contains('Delhi', case=False)] = "India"
+
+df.loc[df['user_location'].str.contains('USA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('United States', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('DC', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('NY', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('CA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('MA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('MD', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('GA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('TX', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('PA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('WA', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('York', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('Texas', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('Los', case=False)] = "United States"
+df.loc[df['user_location'].str.contains('Florida', case=False)] = "United States"
+
+df.loc[df['user_location'].str.contains('Toronto', case=False)] = "Canada"
+
+df.loc[df['user_location'].str.contains('England', case=False)] = "United Kingdom"
+df.loc[df['user_location'].str.contains('UK', case=False)] = "United Kingdom"
+df.loc[df['user_location'].str.contains('London', case=False)] = "United Kingdom"
+
+df.loc[df['user_location'].str.contains('Nigeria', case=False)] = "Nigeria"
+
+df.loc[df['user_location'].str.contains('China', case=False)] = "China"
+df.loc[df['user_location'].str.contains('Kong', case=False)] = "China"
+
+df.loc[df['user_location'].str.contains('Australia', case=False)] = "Australia"
+
+df.loc[df['user_location'].str.contains('Ireland', case=False)] = "Ireland"
+df.loc[df['user_location'].str.contains('Switzerland', case=False)] = "Switzerland"
+
+df.loc[df['user_location'].str.contains('Worldwide', case=False)] = "USELESS"
+df.loc[df['user_location'].str.contains('WORLDWIDE', case=False)] = "USELESS"
+df.loc[df['user_location'].str.contains('Earth', case=False)] = "USELESS"
+df.loc[df['user_location'].str.contains('Global', case=False)] = "USELESS"
+df.loc[df['user_location'].str.contains('Text', case=False)] = "USELESS"
+df.loc[df['user_location'].str.contains('Europe', case=False)] = "USELESS"
+
+
+
+print("No. Of Unique Locations:",df["user_location"].nunique())
+
+
+
+plt.figure(figsize=(10,12))
+sns.barplot(df["user_location"].value_counts().values[0:20],
+            df["user_location"].value_counts().index[0:20]);
+plt.title("Top 15 Location")
+plt.xlabel("No. of tweets")
+plt.ylabel("Location")
+plt.show()
