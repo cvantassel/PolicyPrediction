828e2c0d97f8b57767b09a9c898307897092102c
Conraw <46946984+Conraw@users.noreply.github.com>
2020-11-18 12:51:13 -0600

    Cleanup

--- policyPrediction.py
+++ policyPrediction.py
@@ -33,62 +33,28 @@ sid = SentimentIntensityAnalyzer()
 df["Score"] = df["text_clean"].apply(lambda x: sid.polarity_scores(x)) 
 print(df["Score"])
 
+#Number of random locations listed
 print("No. Of Original Unique Locations:",df["user_location"].nunique())
-
+#dropping all null user locations
 df.dropna(subset=['user_location'], inplace=True)
 
-#if (df['user_location'].__contains__('India')):
-#    df['user_location'] = 'India'
 
-
-df.loc[df['user_location'].str.contains('India', case=False)] = "India"
-df.loc[df['user_location'].str.contains('Delhi', case=False)] = "India"
-
-df.loc[df['user_location'].str.contains('USA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('United States', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('DC', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('NY', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('CA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('MA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('MD', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('GA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('TX', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('PA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('WA', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('York', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('Texas', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('Los', case=False)] = "United States"
-df.loc[df['user_location'].str.contains('Florida', case=False)] = "United States"
-
+#making locations such as New York fall under the United States catagory
+df.loc[df['user_location'].str.contains('India|Delhi', case=False)] = "India"
+df.loc[df['user_location'].str.contains('USA|United States|DC|NY|CA|MA|MD|GA|TX|PA|WA|York|Texas|Los|Florida', case=False)] = "United States"
 df.loc[df['user_location'].str.contains('Toronto', case=False)] = "Canada"
-
-df.loc[df['user_location'].str.contains('England', case=False)] = "United Kingdom"
-df.loc[df['user_location'].str.contains('UK', case=False)] = "United Kingdom"
-df.loc[df['user_location'].str.contains('London', case=False)] = "United Kingdom"
-
+df.loc[df['user_location'].str.contains('England|UK|London', case=False)] = "United Kingdom"
 df.loc[df['user_location'].str.contains('Nigeria', case=False)] = "Nigeria"
-
-df.loc[df['user_location'].str.contains('China', case=False)] = "China"
-df.loc[df['user_location'].str.contains('Kong', case=False)] = "China"
-
+df.loc[df['user_location'].str.contains('China|Kong', case=False)] = "China"
 df.loc[df['user_location'].str.contains('Australia', case=False)] = "Australia"
-
 df.loc[df['user_location'].str.contains('Ireland', case=False)] = "Ireland"
 df.loc[df['user_location'].str.contains('Switzerland', case=False)] = "Switzerland"
+df.loc[df['user_location'].str.contains('Worldwide|WORLD|Earth|Global|Text|Europe', case=False)] = "USELESS"
 
-df.loc[df['user_location'].str.contains('Worldwide', case=False)] = "USELESS"
-df.loc[df['user_location'].str.contains('WORLDWIDE', case=False)] = "USELESS"
-df.loc[df['user_location'].str.contains('Earth', case=False)] = "USELESS"
-df.loc[df['user_location'].str.contains('Global', case=False)] = "USELESS"
-df.loc[df['user_location'].str.contains('Text', case=False)] = "USELESS"
-df.loc[df['user_location'].str.contains('Europe', case=False)] = "USELESS"
-
-
-
+#Number of locations after filtering out the excess
 print("No. Of Unique Locations:",df["user_location"].nunique())
 
-
-
+#graph of the top locations with the number of tweets in each respective country
 plt.figure(figsize=(10,12))
 sns.barplot(df["user_location"].value_counts().values[0:20],
             df["user_location"].value_counts().index[0:20]);
